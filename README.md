26575f7eeb6e110f9fe2635e67c91f947540c54b
vil11 <vilya04@gmail.com>
2019-09-13 23:52:15 +0300

    - [edited] readme - [added] "update Metadata" feature facade

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # bhv
 
-- you may customize formatting & other"app/config/settings.ini"
-
-
-- :information_source: tested on Windows 10 Enterprise x64, PHP7.2
+- :honeybee: available features are described in "app/features.php"
+- :honeybee: you may customize formatting, paths & so on in "app/config/settings.ini", settings values are marked with `*`  in annotations all over the project
+- :point_right: tested on Windows 10 Enterprise x64 + PHP7.2
---
+++ app/features.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * as a Bee, I want to update Metadata of all Artist Songs or for concrete Artist Albums only
+ *  - run manually by calling the function directly from "index.php"
+ *  - start Artist folder name & Album folder name with an underscore* in order to tag it for updating
+ *  - if no Albums are tagged for updating, all Artist Songs will be updated
+ *  - you can enable automatic renaming of tagged folders on operation success, by default this option is disabled
+ *
+ * @param bool $autoRenamingIfSuccess
+ * @throws Exception
+ */
+function updateMetadata(bool $autoRenamingIfSuccess = false)
+{
+    $bhv = new bhv();
+    $result = $bhv->updateMetadata($autoRenamingIfSuccess);
+
+    $result = ($result) ? 'SUCCESS' : 'FAIL!!!';
+    echo "\n\n[$result] finished.\n\n";
+}
