12b316fecb9054d4c18a2000886f1c0d99f912a2
vil11 <vilya04@gmail.com>
2019-09-14 23:12:13 +0300

    - [updated] "hlpr" lib 1.2.2

--- app/lib/hlpr/autoload.php
+++ app/lib/hlpr/autoload.php
@@ -6,7 +6,5 @@ require_once('function/exception.php');
 require_once('function/metadata.php');
 require_once('function/read.php');
 require_once('function/string.php');
-
-// [IN PROGRESS]
 //require_once('function/url.php');
 //require_once('function/zend.php');
