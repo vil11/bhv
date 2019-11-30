bd13e291b01b68801a5751a7fe2cb474dcb90ed8
vil11 <vilya04@gmail.com>
2019-11-30 13:12:55 +0200

    - [updated] "hlpr" lib 1.3.0

--- app/lib/hlpr/function/array.php
+++ app/lib/hlpr/function/array.php
@@ -6,16 +6,18 @@
  * @param array $array
  * @return array
  *
- * @tested 1.2.7
+ * @tested 1.3.0
  */
 function wrap(array $array): array
 {
     $wrapped = [];
     foreach ($array as $key => $value) {
         if (is_int($key)) {
-            $wrapped[] = [$value];
+            $wrapped[$value] = [$value];
         } else {
-            $wrapped[] = [$key, $value];
+//            $wrapped[$key] = [$key, $value];
+//            $wrapped[$key] = [$value];
+            $wrapped[$value] = [$key, $value];
         }
     }
 
