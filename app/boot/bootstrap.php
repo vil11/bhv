e04888c6fa1babc108c9a9f41239ff44ea295362
dimon dominator <vilya04@gmail.com>
2021-03-29 01:30:49 +0300

    dependencies update

--- app/boot/bootstrap.php
+++ app/boot/bootstrap.php
@@ -1,22 +1,22 @@
 <?php
 
 
-// 0. preset system settings
+// preset system settings
 set_time_limit(0);
 ini_set('display_errors', true);
 
 
-// 1. define constants
+// define constants
 define('DS', DIRECTORY_SEPARATOR);
 
 define('PATH_APP', dirname(realpath((__DIR__))) . DS);
 define('PATH_QA', dirname(PATH_APP) . DS . 'qa' . DS);
 define('PATH_VENDOR', dirname(PATH_APP) . DS . 'vendor' . DS);
 
-// 2. upload app settings
+// upload app settings
 require_once(PATH_APP . 'config' . DS . 'settings.php');
 
-// 3. register app & vendor classes
+// register vendor & app classes
 require_once(PATH_VENDOR . 'autoload.php');
 require_once(realpath(__DIR__) . DS . 'autoload.php');
 spl_autoload_register(array('autoloader', 'autoload'));
